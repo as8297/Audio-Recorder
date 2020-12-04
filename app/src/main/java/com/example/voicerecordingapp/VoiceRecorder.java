9f2565a4905e086d3c0ecc88033dfab19ec131df
as8297 <TheAkash@live.com>
2020-12-04 17:53:33 +0530

    RecyclerView Implemented for list and Player interface created.

--- app/src/main/java/com/example/voicerecordingapp/VoiceRecorder.java
+++ app/src/main/java/com/example/voicerecordingapp/VoiceRecorder.java
@@ -2,57 +2,29 @@ package com.example.voicerecordingapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VoiceRecorder#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class VoiceRecorder extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class VoiceRecorder extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private NavController navController;
+    private ImageButton listBtn;
+    private ImageButton recordBtn;
+
+    private boolean isRecording = false;
+
 
     public VoiceRecorder() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VoiceRecorder.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VoiceRecorder newInstance(String param1, String param2) {
-        VoiceRecorder fragment = new VoiceRecorder();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +32,39 @@ public class VoiceRecorder extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_voice_recorder, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        listBtn = view.findViewById(R.id.record_list_btn);
+        recordBtn = view.findViewById(R.id.record_btn);
+
+        listBtn.setOnClickListener(this);
+        recordBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.record_list_btn:
+                navController.navigate(R.id.action_voiceRecorder_to_recordPlayer);
+                break;
+
+            case R.id.record_btn:
+                if(isRecording) {
+                    //Stop Recording
+                    recordBtn.setImageDrawable(getResources().getDrawable(R.drawable.record_btn_stopped, null));
+                    isRecording = false;
+                } else {
+                    //Start Recording
+                    recordBtn.setImageDrawable(getResources().getDrawable(R.drawable.record_btn_recording, null));
+                    isRecording = true;
+                }
+                break;
+        }
+
     }
 }
